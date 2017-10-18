a271810a7656ae316a235a88ce3bea57225889a7
Dennis Wells <aadennis@users.noreply.github.com>
2017-10-18 23:04:21 +0000

    Sandbox for MTM dev

---
+++ MtmSandbox/MtmSandbox/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Framework.Common;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		const string TfsUrl = "https://dennis.visualstudio.com";
+		static void Main(String[] args) {
+			var serverUrl = new Uri(TfsUrl);
+
+			var configurationServer =
+				TfsConfigurationServerFactory.GetConfigurationServer(serverUrl);
+
+			// Get the catalog of team project collections
+			var collectionNodes = configurationServer.CatalogNode.QueryChildren(
+				new[] { CatalogResourceTypes.ProjectCollection },
+				false, CatalogQueryOptions.None);
+
+			// List the team project collections
+			foreach (var collectionNode in collectionNodes) {
+				// Use the InstanceId property to get the team project collection
+				var collectionId = new Guid(collectionNode.Resource.Properties["InstanceId"]);
+				var teamProjectCollection = configurationServer.GetTeamProjectCollection(collectionId);
+
+				// Print the name of the team project collection
+				Console.WriteLine("Collection: " + teamProjectCollection.Name);
+
+				// Get a catalog of team projects for the collection
+				var projectNodes = collectionNode.QueryChildren(
+					new[] { CatalogResourceTypes.TeamProject },
+					false, CatalogQueryOptions.None);
+
+				// List the team projects in the collection
+				foreach (var projectNode in projectNodes) {
+					Console.WriteLine(" Team Project: " + projectNode.Resource.DisplayName);
+				}
+			}
+			Console.WriteLine("Done. Press any key");
+			Console.ReadKey();
+		}
+	}
+}
+
+
+
