3b3cfa92ed62637d6bdcd138fcf5ae1db24965c2
Dennis Wells <aadennis@users.noreply.github.com>
2017-10-19 20:18:37 +0000

    Refactored before doing another way

--- MtmSandbox/MtmSandbox/Program.cs
+++ MtmSandbox/MtmSandbox/Program.cs
@@ -1,46 +1,56 @@
 ﻿using System;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.Framework.Common;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
 
 namespace ConsoleApp1
 {
 	class Program
 	{
 		const string TfsUrl = "https://dennis.visualstudio.com";
-		static void Main(String[] args) {
-			var serverUrl = new Uri(TfsUrl);
+		static void Main(String[] args)
+        {
+            var serverUrl = new Uri(TfsUrl);
 
-			var configurationServer =
-				TfsConfigurationServerFactory.GetConfigurationServer(serverUrl);
+            GetProjectsForServerV1(serverUrl);
+            Console.WriteLine("Done. Press any key");
+            Console.ReadKey();
+        }
 
-			// Get the catalog of team project collections
-			var collectionNodes = configurationServer.CatalogNode.QueryChildren(
-				new[] { CatalogResourceTypes.ProjectCollection },
-				false, CatalogQueryOptions.None);
+        private static void GetProjectsForServerV1(Uri serverUrl)
+        {
+            var configurationServer =
+                            TfsConfigurationServerFactory.GetConfigurationServer(serverUrl);
 
-			// List the team project collections
-			foreach (var collectionNode in collectionNodes) {
-				// Use the InstanceId property to get the team project collection
-				var collectionId = new Guid(collectionNode.Resource.Properties["InstanceId"]);
-				var teamProjectCollection = configurationServer.GetTeamProjectCollection(collectionId);
+            // Get the catalog of team project collections
+            var collectionNodes = configurationServer.CatalogNode.QueryChildren(
+                new[] { CatalogResourceTypes.ProjectCollection },
+                false, CatalogQueryOptions.None);
 
-				// Print the name of the team project collection
-				Console.WriteLine("Collection: " + teamProjectCollection.Name);
+            // List the team project collections
+            foreach (var collectionNode in collectionNodes)
+            {
+                // Use the InstanceId property to get the team project collection
+                var collectionId = new Guid(collectionNode.Resource.Properties["InstanceId"]);
+                var teamProjectCollection = configurationServer.GetTeamProjectCollection(collectionId);
 
-				// Get a catalog of team projects for the collection
-				var projectNodes = collectionNode.QueryChildren(
-					new[] { CatalogResourceTypes.TeamProject },
-					false, CatalogQueryOptions.None);
+                // Print the name of the team project collection
+                Console.WriteLine("Collection: " + teamProjectCollection.Name);
 
-				// List the team projects in the collection
-				foreach (var projectNode in projectNodes) {
-					Console.WriteLine(" Team Project: " + projectNode.Resource.DisplayName);
-				}
-			}
-			Console.WriteLine("Done. Press any key");
-			Console.ReadKey();
-		}
-	}
+                // Get a catalog of team projects for the collection
+                var projectNodes = collectionNode.QueryChildren(
+                    new[] { CatalogResourceTypes.TeamProject },
+                    false, CatalogQueryOptions.None);
+
+                // List the team projects in the collection
+                foreach (var projectNode in projectNodes)
+                {
+                    Console.WriteLine(" Team Project: " + projectNode.Resource.DisplayName);
+
+                }
+            }
+        }
+    }
 }
 
 
