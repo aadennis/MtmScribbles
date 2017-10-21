03938082984d99b56acd35e07becdbd87f9179f9
Dennis Wells <aadennis@users.noreply.github.com>
2017-10-21 19:31:49 +0000

    "rest" api usage to return projects and test plans

---
+++ Tryouts/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Configuration;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace ConsoleApp2 {
+
+    public static class JsonHelper {
+        public static string FormatJson(string json) {
+            return JValue.Parse(json).ToString(Formatting.Indented);
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            GetTfsProjects();
+            Console.WriteLine("Done. Press a key");
+            Console.ReadKey();
+        }
+
+        // https://docs.microsoft.com/en-us/vsts/integrate/get-started/rest/basics
+        // https://docs.microsoft.com/en-us/vsts/integrate/get-started/client-libraries/samples
+        // https://docs.microsoft.com/en-us/vsts/integrate/quickstarts/work-item-quickstart
+        public static async void GetTfsProjects() {
+            try {
+                var personalAccessToken = ConfigurationManager.AppSettings["PAT"];
+                // Note that tfsServer has a format something like this: "joe.visualstudio.com"
+                var tfsServer = ConfigurationManager.AppSettings["TfsServer"];
+
+                using (var client = new HttpClient()) {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                        Convert.ToBase64String(
+                            System.Text.Encoding.ASCII.GetBytes(
+                                string.Format("{0}:{1}", "", personalAccessToken))));
+
+                    using (var response = client.GetAsync(
+                                $"{tfsServer}/DefaultCollection/_apis/projects").Result) {
+                        response.EnsureSuccessStatusCode();
+                        var responseBody = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(JsonHelper.FormatJson(responseBody));
+
+                    }
+                    using (var response2 = client.GetAsync(
+                                $"{tfsServer}/DefaultCollection/ARM Basic Templates/_apis/test/plans?api-version=1.0").Result) {
+                        response2.EnsureSuccessStatusCode();
+                        var responseBody2 = await response2.Content.ReadAsStringAsync();
+                        Console.WriteLine(JsonHelper.FormatJson(responseBody2));
+
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
